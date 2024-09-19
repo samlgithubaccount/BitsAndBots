3e971ffe1d686affeeeadf8e9939efe5b4649102
Duc Le <u3235091@uni.canberra.edu.au>
2024-09-19 13:29:28 +1000

    Login/Register additions

--- BitsAndBots/Data/ApplicationUser.cs
+++ BitsAndBots/Data/ApplicationUser.cs
@@ -7,6 +7,14 @@ namespace BitsAndBots.Data
     public class ApplicationUser : IdentityUser
     {
         public ICollection<Product> Products { get; set; } = [];
+
+        public string FirstName { get; set; } = "";
+
+        public string LastName { get; set; } = "";
+
+        public string PhoneNumber { get; set; } = "";
+
+
     }
 
 }
