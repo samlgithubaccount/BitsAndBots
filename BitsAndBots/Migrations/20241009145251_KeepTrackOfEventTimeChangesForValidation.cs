e19caf79f2c875c1d2698ca286ba378149396d3c
samlgithubaccount <samuell.public@gmail.com>
2024-10-10 02:28:59 +1100

    Add footer text Fix fundraiser reg validations Halfway complete prevent validations on unchanged start/end times Validate quantity is less than quantity

---
+++ BitsAndBots/Migrations/20241009145251_KeepTrackOfEventTimeChangesForValidation.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BitsAndBots.Migrations
+{
+    /// <inheritdoc />
+    public partial class KeepTrackOfEventTimeChangesForValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Fundraiser",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Fundraiser",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FundraiserLink",
+                table: "Fundraiser",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Fundraiser",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExistingEndTime",
+                table: "Event",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExistingStartTime",
+                table: "Event",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExistingEndTime",
+                table: "Event");
+
+            migrationBuilder.DropColumn(
+                name: "ExistingStartTime",
+                table: "Event");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Fundraiser",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Fundraiser",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FundraiserLink",
+                table: "Fundraiser",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Fundraiser",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000);
+        }
+    }
+}
--- BitsAndBots/Validators/FutureDateTimeValidator.cs
+++ BitsAndBots/Validators/FutureDateTimeValidator.cs
@@ -4,20 +4,29 @@ namespace BitsAndBots.Validators
 {
     public class FutureDateTimeValidator : ValidationAttribute
     {
-        public override bool IsValid(object? value)
+        private readonly string existingTimePropertyName;
+        public FutureDateTimeValidator(string existingTimePropertyName)
+        {
+            this.existingTimePropertyName = existingTimePropertyName;
+        }
+
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
         {
             if (value == null)
             {
-                return true;
+                return ValidationResult.Success;
             }
+
+            var existingTimeProperty = validationContext.ObjectType.GetProperty(existingTimePropertyName);
+            var existingTime = (DateTime)existingTimeProperty.GetValue(validationContext.ObjectInstance);
 
             var dateTime = (DateTime)value;
 
-            if (dateTime <= DateTime.Now)
+            if (dateTime != existingTime && dateTime <= DateTime.Now)
             {
-                return false;
+                return new ValidationResult($"{validationContext.DisplayName} must be in the future.", new List<string> { validationContext.MemberName });
             }
-            return true;
+            return ValidationResult.Success;
         }
     }
 }
