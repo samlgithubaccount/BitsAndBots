ab4fb779c027c17b7ade241e4b3a4494f93cc15f
samlgithubaccount <samuell.public@gmail.com>
2024-10-13 01:36:05 +1100

    Fix bug with tags containing whitespace not being searchable by banning whitespace

---
+++ BitsAndBots/Validators/TagValidatior.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace BitsAndBots.Validators
+{
+    public class TagValidator : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            return !Regex.Match(value.ToString(), @"\s").Success;
+        }
+    }
+}
